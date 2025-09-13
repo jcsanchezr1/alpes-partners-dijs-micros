19cae1e2800cd00e64f6be2dbc368f968e563570
Diego Jaramillo <df.jaramilloa1@uniandes.edu.co>
2025-09-12 23:11:07 -0500

    ajustes puertos

--- campanas/run_flask.py
+++ campanas/run_flask.py
@@ -11,4 +11,4 @@ sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'src'))
 from alpes_partners.main import app
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=8000)
+    app.run(debug=True, host='0.0.0.0', port=8080)
